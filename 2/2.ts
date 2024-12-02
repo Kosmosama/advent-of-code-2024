ace3d8c1212c261fb2cfdf9c62278c19798596df
Kosmosama <luismg0408@gmail.com>
2024-12-02 21:12:43 +0100

    Feat: Day 2 - 2 star

--- 2/2.ts
+++ 2/2.ts
@@ -1,10 +1,10 @@
 const resultSpan = document.getElementById('result') as HTMLElement;
 const dataInput = document.getElementById('inputData') as HTMLTextAreaElement;
 
+// Puzzle 1 ------------------------------------------------------------------------------
 function calculateTotalSafe(inputData: number[][]): number {
     function isSafe(list: number[]): boolean {
         if (list.length < 2) return false;
-        
         const direction = list[1] - list[0];
         if (Math.abs(direction) < 1 || Math.abs(direction) > 3) return false;
     
@@ -20,12 +20,31 @@ function calculateTotalSafe(inputData: number[][]): number {
 
     return inputData.reduce((total, value) => total + (isSafe(value) ? 1 : 0), 0);
 }
+//---------------------------------------------------------------------------------------
 
+// Puzzle 2 ------------------------------------------------------------------------------
 function calculateTotalSafeWithDampener(inputData: number[][]): number {
-    // const { list1, list2 } = parseInputToLists(inputData);
-    // return list1.reduce((total, value) => total + (list2.filter(number => number === value).length * value), 0);
-    return 1;
+    function isSafe(list: number[]): boolean {
+        if (list.length < 2) return false;
+        const direction = list[1] - list[0];
+        let errors = 0;
+    
+        return list.slice(1).every((value, index) => {
+            const diff = value - list[index];
+    
+            const isValid =
+                Math.abs(diff) >= 1 &&
+                Math.abs(diff) <= 3 &&
+                (direction > 0 ? diff > 0 : diff < 0);
+    
+            if (!isValid) errors++;
+            return errors <= 1; // Allow only one error
+        });
+    }
+
+    return inputData.reduce((total, value) => total + (isSafe(value) ? 1 : 0), 0);
 }
+//---------------------------------------------------------------------------------------
 
 function parseInputToLists(inputData: string): number[][] {
     return inputData
